ef960a11cf9bf1f810fbe6b6f4346be06c68973f
conquer-bok <ksngee4@naver.com>
2025-01-10 21:47:29 +0900

    Add files via upload

--- Home_app/models.py
+++ Home_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    CATEGORY_CHOICES = [
+        ('notice', '공지사항'),
+        ('suggestion', '건의사항'),
+        ('seminar', '세미나'),
+    ]
+
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)  # 카테고리 추가
+    image = models.ImageField(upload_to='images/', blank=True, null=True)  # 이미지 업로드
+    file = models.FileField(upload_to='files/', blank=True, null=True)  # 파일 업로드
+
+    def __str__(self):
+        return self.title
--- Home_app/urls.py
+++ Home_app/urls.py
@@ -16,10 +16,43 @@ Including another URLconf
 """
 from django.urls import path, include
 from Home_app import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'Home_app'
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('Home', views.Home, name='Home'),
+    path('Found', views.Found, name='Found'),
+    path('Member', views.Member, name='Member'),
+    path('Suggest', views.Suggest, name='Suggest'),
+    path('Notice', views.Notice, name='Notice'),
+    path('Seminar', views.Seminar, name='Seminar'),
+    path('Notice_write', views.Notice_write, name='Notice_write'),
+    path('Notice_detail', views.Notice_detail, name='Notice_detail'),
+    path('Notice_edit', views.Notice_edit, name='Notice_edit'),
+    path('Notice_write', views.Notice_write, name='Notice_write'),
+    path('Notice/<int:post_id>', views.Notice_detail, name='Notice_detail'),
+    path('Notice/<int:post_id>/edit/', views.Notice_edit, name='Notice_edit'),  # 수정 페이지
+    path('Notice/<int:post_id>/delete/', views.Notice_delete, name='Notice_delete'),  # 삭제 페이지
+    path('Idea', views.Idea, name='Idea'),
+    path('Team', views.Team, name='Team'),
+    path('Suggest_write', views.Suggest_write, name='Suggest_write'),
+    path('Suggest_detail', views.Suggest_detail, name='Suggest_detail'),
+    path('Suggest_edit', views.Suggest_edit, name='Suggest_edit'),
+    path('Suggest_write', views.Suggest_write, name='Suggest_write'),
+    path('Suggest/<int:post_id>', views.Suggest_detail, name='Suggest_detail'),
+    path('Suggest/<int:post_id>/edit/', views.Suggest_edit, name='Suggest_edit'),  # 수정 페이지
+    path('Suggest/<int:post_id>/delete/', views.Suggest_delete, name='Suggest_delete'),  # 삭제 페이지
+    path('Seminar_write', views.Seminar_write, name='Seminar_write'),
+    path('Seminar_detail', views.Seminar_detail, name='Seminar_detail'),
+    path('Seminar_edit', views.Seminar_edit, name='Seminar_edit'),
+    path('Seminar_write', views.Seminar_write, name='Seminar_write'),
+    path('Seminar/<int:post_id>', views.Seminar_detail, name='Seminar_detail'),
+    path('Seminar/<int:post_id>/edit/', views.Seminar_edit, name='Seminar_edit'),  # 수정 페이지
+    path('Seminar/<int:post_id>/delete/', views.Seminar_delete, name='Seminar_delete'),  # 삭제 페이지
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
